d18097b92912dfec41635fbdde5322419ac70d64
Yan Karpov <oficialbalunov@gmail.com>
2020-10-16 01:59:06 +0800

    Исправил числового бота

--- src/js/main.js
+++ src/js/main.js
@@ -7,31 +7,41 @@ let numberBot = function() {
   let isNumber = function(n) {
     return !isNaN(parseFloat(n)) && isFinite(n);
   };
-
-  let answer = prompt('Угадай число от 1 до 100');
   
-  function getRandomNumber(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; 
-  }
+  return function repeat() {
 
-  let randomNumber = getRandomNumber(1, 100);
-  console.log(randomNumber);
+    function getRandomNumber(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min; 
+    }
 
-  if (answer === null) {
-    alert('Игра окончена');
-  } else if (!isNumber(answer)) {
-    alert('Введи число!');
-    numberBot();
-  } else if (answer === randomNumber) {
-    alert('Поздравляю, Вы угадали!!!');
-  } else if (answer > randomNumber) {
-    alert('Загаданное число меньше');
-  } else if (answer < randomNumber) {
-    alert('Загаданное число больше');
-  } 
+    let randomNumber = getRandomNumber(1, 100);
+    
+    let answer = prompt('Угадай число от 1 до 100');
 
+    if (answer === null) {
+      alert('Игра окончена');
+    } else if (!isNumber(answer)) {
+      alert('Введи число!');
+      return repeat();
+    } else if (answer === randomNumber) {
+      console.log(randomNumber);
+      alert('Поздравляю, Вы угадали!!!');
+      let answerTwo = confirm('Хотите сыграть еще?');
+      if (answerTwo === 1){
+        return repeat();
+      }
+    } else if (answer > randomNumber) {
+      console.log(randomNumber);
+      alert('Загаданное число меньше');
+      return repeat();
+    } else if (answer < randomNumber) {
+      console.log(randomNumber);
+      alert('Загаданное число больше');
+      return repeat();
+    } 
+  };
 };
 
-numberBot();
+numberBot()();
